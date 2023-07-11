c4d9b9695a8b94b272577255e4e927913176a3a5
Omar Hafez <omarhafez@Omars-MacBook-Pro-2.local>
2023-07-11 19:03:15 -0400

    todoapp

---
+++ src/components/AppContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppContent() {
+  return (
+    <div>
+      <h1>Hello from App Content</h1>
+    </div>
+  );
+}
+
+export default AppContent;
---
+++ src/components/AppHeader.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Button, { SelectButton } from './Button';
+import styles from '../styles/modules/app.module.scss';
+import ToDoModal from './ToDoModal';
+
+function AppHeader() {
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <div className={styles.appHeader}>
+      <Button variant="primary" onClick={() => setModalOpen(true)}>
+        Add Task
+      </Button>
+      <SelectButton id="status">
+        <option value="all">All</option>
+        <option value="incomplete">Incomplete</option>
+        <option value="complete">complete</option>
+      </SelectButton>
+      <ToDoModal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+    </div>
+  );
+}
+
+export default AppHeader;
